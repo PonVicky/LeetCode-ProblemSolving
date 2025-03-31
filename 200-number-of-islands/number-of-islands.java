cef1753117648844eb2cc1e0902856ef75d50adc
PonVignesh <98687019+PonVicky@users.noreply.github.com>
2025-03-31 10:41:20 +0530

    Time: 3 ms (86.24%) | Memory: 49 MB (90.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,27 +1,26 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int numberOfIslands = 0;
-        int n  = grid.length;
-        int m = grid[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+    
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j] == '1'){
-                    searchIslands(i,j,grid,n,m);
+                    searchIslands(i,j,grid);
                     numberOfIslands++;
                 }
             }
         }
         return numberOfIslands;
     }
-    public static void searchIslands(int row, int col, char[][] arr,int n, int m){
-        if(row < 0 || row >=n || col < 0 || col >= m||
+    public static void searchIslands(int row, int col, char[][] arr){
+        if(row < 0 || row >=arr.length|| col < 0 || col >= arr[0].length||
             arr[row][col] != '1'){
                 return;
         }
         arr[row][col] = '0';
-        searchIslands(row+1,col,arr,n,m);
-        searchIslands(row-1,col,arr,n,m);
-        searchIslands(row,col+1,arr,n,m);
-        searchIslands(row,col-1,arr,n,m);
+        searchIslands(row+1,col,arr);
+        searchIslands(row-1,col,arr);
+        searchIslands(row,col+1,arr);
+        searchIslands(row,col-1,arr);
     }
 }
