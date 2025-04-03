487658dacc9675017e46a267fb6954b61054c974
PonVignesh <98687019+PonVicky@users.noreply.github.com>
2025-04-03 14:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (63.42%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,21 +1,12 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        int len = s.length();
-        if(len == 2){
-            return s.charAt(0);
-        }
-        
-        int[] arr = new int[26];
-        char out  = ' ';
-        
-        for(int i=0;i<len;i++){
+        int[] hash = new int[26];
+        for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(arr[ch-'a']==1){
-                out = ch;
-                break;
-            }
-            arr[ch-'a']++;
+            if(hash[ch-'a']!=0) return ch;
+            hash[ch-'a']++;
         }
-        return out;
+
+        return 'f';
     }
 }
