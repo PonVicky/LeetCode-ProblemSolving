54e31f3509fc6d1ab45e53aa952da338a624019d
PonVignesh <98687019+PonVicky@users.noreply.github.com>
2025-04-16 00:00:17 +0530

    Time: 7 ms (88.27%) | Memory: 57 MB (50.57%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -8,35 +8,13 @@ class Solution {
                 if((i==0||j==0||i==n-1||j==m-1 ) && grid[i][j] == 1) dfs(i,j,grid);
             }
         }
-        int[] count  = {0};
+        int count  = 0;
         for(int i=1;i<n-1;i++){
             for(int j=1;j<m-1;j++){
-                if(grid[i][j] == 1){
-                    dfs(i,j,grid,count);
-                }
+                if(grid[i][j] == 1 && !visited[i][j]) count++;
             }
         }
-
-        return count[0];
-    }
-
-    public static void dfs(int row, int col, int[][] arr, int[] count){
-         if(row < 0 || row >= arr.length || col < 0 || col >= arr[0].length 
-        || arr[row][col] == 0 || visited[row][col]){
-            return;
-        }
-        visited[row][col] = true;
-        if(arr[row][col] == 1 && !(row==0 || col == 0 || row == arr.length-1 || col == arr[0].length-1)){
-            count[0]++;
-        }
-        arr[row][col] =  0;
-
-        dfs(row+1,col,arr,count);
-        dfs(row,col+1,arr,count);
-        dfs(row-1,col,arr,count);
-        dfs(row,col-1,arr,count);
-
-        visited[row][col] = false;
+        return count;
     }
 
     public static void dfs(int row, int col, int[][] arr){
@@ -44,7 +22,6 @@ class Solution {
         || arr[row][col] == 0 || visited[row][col]){
             return;
         }
-
 
         visited[row][col] = true;
         arr[row][col] =  0;
