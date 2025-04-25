a9cd5195e51247de2d18ba4fb54ed567d962169e
PonVignesh <98687019+PonVicky@users.noreply.github.com>
2025-04-25 16:28:50 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (47.72%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = firstPosition(nums,target);
-        int[] arr = new int[2];
-        if(first  == -1){
-            arr[0] = -1;arr[1] = -1;
-            return arr;
-        }
-        arr[0] = first;
-        arr[1] = lastPosition(nums,target);
-
-        return arr;
+        int first = lowerBound(target,nums);
+        if(first == nums.length || nums[first] != target) return new int[] {-1,-1};
+        return new int[] {first,upperBound(target,nums)-1};
     }
-
-    public static int lastPosition(int[] arr, int x){
-        int index  = -1;
-        int low = 0 , high = arr.length-1;
+    public static int upperBound(int target, int[] arr){
+        int found  = arr.length;
+        int low  = 0, high = arr.length-1;
         while(low <= high){
-            int mid = low + (high - low)/2;
-            if(arr[mid] == x){
-                index = mid;
+            int mid = (low + high)/2;
+            if(arr[mid] > target){
+                found = mid;
+                high = mid -1;
+            }else{
                 low = mid +1;
-            }else if(arr[mid] > x){
-                high = mid-1;
-            }else low = mid +1;
+            }
         }
-        return index;
+        return found;
     }
-
-    public static int firstPosition(int[] arr, int x){
-        int index  = -1;
-        int low =0, high = arr.length-1;
+    public static int lowerBound(int target, int[] arr){
+        int found  = arr.length;
+        int low  = 0 , high = arr.length-1;
         while(low <= high){
-            int mid = low + (high-low)/2;
-            if(arr[mid] == x ){
-                index = mid;
-                high = mid -1;
-            }else if(arr[mid] > x){
+            int mid = (low + high)/2;
+            if(arr[mid] >= target){
+                found  = mid;
                 high = mid -1;
             }else low = mid +1;
         }
-        return index;
+        return found;
     }
-
 
 }
