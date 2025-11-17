965110bf8abe3c7c619339018123c8a389f8c236
PonVignesh <98687019+PonVicky@users.noreply.github.com>
2025-11-17 08:50:03 +0530

    Time: 0 ms (100.00%) | Memory: 116 MB (18.15%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,33 +1,32 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int[] arr= {-1,-1};
         int n = mat.length, m = mat[0].length;
-        int low  = 0, high  = m-1;
+        int low  = 0, high = m-1;
         while(low <= high){
-            int mid = low + (high - low)/2;
-            int i = findI(mat,n,mid);
-            int left  = -1, right  = -1;
-            if(mid!=0) left = mat[i][mid-1];
-            if(mid!=m-1) right = mat[i][mid+1]; 
-            if(mat[i][mid] > left && mat[i][mid] > right){
-                arr[0] = i;
-                arr[1] = mid;
-                return arr;
-            }else if(mat[i][mid] < left) high = mid -1;
-            else low = mid +1;
-        }
-
-        return arr;
-        
-    }
-    private static int findI(int[][] arr, int n, int j){
-        int max = Integer.MIN_VALUE, ind = 0 ;
-        for(int i=0;i<n;i++){
-            if(arr[i][j] > max){
-                max = arr[i][j];
-                ind = i;
+            int col = (low + high)/2;
+            int row = findRow(mat,n,col);
+            int left = col > 0 ? mat[row][col-1] : -1,  right = col < m-1 ? mat[row][col+1] : -1;
+            if(mat[row][col] > left && mat[row][col] > right){
+                return new int[]{row,col};
             }
-        }  
-        return ind;
+            if(mat[row][col] < left){
+                high= col -1;
+            }else if(mat[row][col] < right){
+                low = col +1;
+            }
+
+        }
+        return new int[] {-1,-1};
+    }
+    public static int findRow(int[][] arr, int n , int col){
+        int max =  -1;
+        int index  = -1;
+        for(int i=0;i<n;i++){
+            if(arr[i][col] > max){
+                max = arr[i][col];
+                index = i;
+            }
+        }
+        return index;
     }
 }
