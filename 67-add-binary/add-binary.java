c9813ac9383b36554b55643af528f7ab737c2b7d
PonVignesh <98687019+PonVicky@users.noreply.github.com>
2026-01-08 14:21:37 +0530

    Time: 2 ms (48.37%) | Memory: 43.6 MB (41.36%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -7,16 +7,8 @@ class Solution {
             int num1  = i >= 0 ? a.charAt(i) -'0' : 0;
             int num2  = j >= 0 ? b.charAt(j) -'0' : 0;
             int sum = num1 + num2 + carry;
-            if(sum == 2){
-                output.append("0");
-                carry = 1;
-            }else if(sum == 3){
-                output.append("1");
-                carry = 1;
-            }else{
-                output.append(sum);
-                carry = 0;
-            }
+             output.append(sum % 2);
+            carry = sum / 2;
             i--;j--; 
         }
 
